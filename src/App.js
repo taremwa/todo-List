282a95c6dfa8252907d676ea27a7d192e47b9916
taremwa <marktaremwa92@gmail.com>
2018-04-30 15:43:59 +0300

    todo-list

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import uuid  from 'uuid';
+import Projects from './components/Projects'; 
+import AddProject from './components/AddProject'; 
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+  componentWillMount(){
+    this.setState({projects: [
+      {
+        id:uuid.v4(),
+        title: 'Bussiness Website',
+        category: 'Web Design'
+      },
+      {
+        id:uuid.v4(),
+        title: 'Social App',
+        category: 'Mobile Development'
+      },
+      {
+        id:uuid.v4(),
+        title: 'Ecommerce Shopping cart',
+        category: 'Web Development'
+      }
+    ]});
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <AddProject addProject={this.handleAddProject.bind(this)} />
+        <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+      </div>
+    );
+  }
+}
+
+export default App;
